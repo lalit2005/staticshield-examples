2cc1a8dbc5ef2e4744220f3040df5e8e24ed882e
Lalit <lalitvijay9480@gmail.com>
2021-06-22 19:46:02 +0530

    feat: add vuejs example

---
+++ with-vuejs/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/banner.png" />
+  <HelloWorld msg="StaticShield + Vuejs = 💚" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  max-width: 500px;
+}
+</style>
---
+++ with-vuejs/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <p class="subtitle">
+      You just visited a secret zone on internet which is password protected 🤯
+    </p>
+
+    <p>
+      For a guide and recipes on how to password protect any Vue.Js app<br />
+      check out the
+      <a
+        href="https://staticshield.vercel.app/docs/vuejs"
+        target="_blank"
+        rel="noopener"
+        >StaticShield + Vuejs documentation</a
+      >.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 50px;
+}
+p {
+  font-size: 22px;
+}
+p.subtitle {
+  font-size: 18px;
+  opacity: 0.8;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
