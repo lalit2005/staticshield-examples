076d7c40e5e501239bbdb7cff329f862d777e920
Lalit <lalitvijay9480@gmail.com>
2021-06-23 10:47:59 +0530

    feat: add nuxtjs example

---
+++ with-nuxtjs/pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container min-h-screen max-w-2xl mx-auto text-center">
+    <div class="max-w-2xl mx-auto">
+      <img src="/banner.png" alt="" class="max-w-md mx-auto mt-32 mb-16" />
+      <h1 class="title font-extrabold text-5xl my-10">
+        StaticShield + Nuxtjs = 💚
+      </h1>
+      <p class="text-xl">
+        Click the link below to visit a page that has been password protected by
+        StaticShield
+      </p>
+      <nuxt-link
+        to="/protected"
+        class="
+          text-blue-500 text-2xl
+          hover:text-blue-700 hover:underline
+          my-10
+          block
+        "
+        >Visit password protected page &rarr;</nuxt-link
+      >
+      <div class="links">
+        <a
+          href="https://staticshield.vercel.app/docs/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          StaticShield Documentation
+        </a>
+        <a
+          href="https://github.com/lalit2005/staticshield-examples"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          Github
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'StaticShield + Nuxtjs',
+    }
+  },
+}
+</script>
---
+++ with-nuxtjs/pages/protected.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="max-w-2xl min-h-screen mx-auto staticshield-div">
+    <img src="/banner.png" alt="" class="max-w-md mx-auto mt-32 mb-16" />
+    <h1 class="text-3xl font-bold text-center">
+      You just visited a secret place in internet which is password protected 🤯
+    </h1>
+
+    <div class="mt-10 text-center links">
+      <a
+        href="https://staticshield.vercel.app/docs/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button--green"
+      >
+        StaticShield Documentation
+      </a>
+      <a
+        href="https://github.com/lalit2005/staticshield-examples"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button--grey"
+      >
+        Github
+      </a>
+    </div>
+    <nuxt-link
+      to="/"
+      class="
+        block
+        my-10
+        text-2xl text-blue-500
+        hover:text-blue-700 hover:underline
+      "
+    >
+      &larr; Go Back
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'StaticShield + Nuxtjs',
+      script: [
+        {
+          hid: 'staticshield',
+          src: 'http://localhost:1337/script.js',
+          'data-site-id': 'fbe1e7da-d79b-4265-8209-a14f099d838e',
+          'data-cap': 'The password is 123test123',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.staticshield-div {
+  display: none;
+}
+</style>
